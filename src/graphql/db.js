51bbb10ea6bb5bdb36a264711a1f5cdeec4357e4
b0ho <ybg4828@gmail.com>
2019-04-18 17:49:39 +0900

    set

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 const BASE_URL = "https://yts.am/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
