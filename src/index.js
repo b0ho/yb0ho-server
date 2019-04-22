087737f84508bc821f45fa91c112550229787f70
b0ho <ybg4828@gmail.com>
2019-04-22 20:10:44 +0900

    config 모듈 추가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 import mongoose from "mongoose";
+import config from "config";
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  "mongodb://yb0ho:soatp1202!@ds223343.mlab.com:23343/heroku_t408d75j",
-  // mLab URL 삽입 dbuser: 아이디, dbpassword: 비밀번호
+  config.db,
 
   { useNewUrlParser: true }
 );
